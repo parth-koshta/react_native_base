438cd4c37b4037c4b62def5babf0e880a7cae7f0
Parth <parth@urbanpiper.com>
2021-12-25 21:12:34 +0530

    - configuring .env using react-native-config

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Config from "react-native-config";
 
 const App = () => {
+  const isProduction = Config.PRODUCTION === 'true';
   return (
     <View style={{ flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 22, fontWeight: '500' }}>React Native Base</Text>
+      <Text style={{ fontSize: 18, fontWeight: '500', marginTop: 20 }}>
+        Running {!!isProduction ? 'Production' : 'Staging'} flavour
+      </Text>
     </View>
   )
 }
