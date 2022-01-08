b578ca5a35f9abeb4c515ca2e8b39a8806941111
Parth <parth@urbanpiper.com>
2022-01-09 00:41:39 +0530

    - added splash screen

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
+import RNBootSplash from 'react-native-bootsplash';
 import store, {persistor} from './src/redux/store';
 import AppWrapper from './src/components/AppWrapper';
 import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      RNBootSplash.hide({fade: true});
+    }, 300);
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
